2aa0fcb7a640c243b91f824ead939de9b7bbe69f
kadeksinduarta <sindu.arta@student.undiksha.ac.id>
2025-11-04 12:32:57 +0800

    chore: add public frontend page on cors whitelist

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://balinesescaredpurification.com'
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
