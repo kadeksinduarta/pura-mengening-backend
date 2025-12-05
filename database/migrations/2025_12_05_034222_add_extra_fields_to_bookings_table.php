91e3594d977aa2d78e0d9dc20b8cc8ba862f7584
kadeksinduarta <sindu.arta@student.undiksha.ac.id>
2025-12-05 12:53:51 +0800

    update after migrate

--- database/migrations/2025_12_05_034222_add_extra_fields_to_bookings_table.php
+++ database/migrations/2025_12_05_034222_add_extra_fields_to_bookings_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('bookings', function (Blueprint $table) {
-            $table->string('order_id')->nullable()->after('price');
+            $table->string('order_id')->nullable()->after('tipe');
         });
     }
 
